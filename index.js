0faf0b5e807ab6beca181f20ca5852c6d88de709
mahin-wd <wdmahin.mhm@gmail.com>
2022-11-01 20:56:11 +0600

    basic mongodb setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,6 +12,13 @@ app.get('/', (req, res) => {
     res.send('Genius Car Server is running');
 })
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4lqljgn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 app.listen(port, () => {
-    condole.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 })
